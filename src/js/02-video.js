10e8aacdaa1bc2182bd957f7fad9ee569f733583
Zhvanko Anna <zhvankoann@gmail.com>
2023-01-11 17:42:44 +0200

    Add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player');
+
+function handleVideoTimeUpdate (e) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(e.seconds))
+}
+
+player.on('timeupdate', throttle(handleVideoTimeUpdate, 1000));
+
+player.setCurrentTime( localStorage.getItem("videoplayer-current-time"))
